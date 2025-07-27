c30776c2db2e43641d6f84dcff4aeb60a0b10df5
Arko2016 <arkojyoti.hore@gmail.com>
2025-07-26 23:48:27 -0700

    created a virtual env

---
+++ frontend.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import requests
+
+#specify the FastAPI URL containing the predict path
+#Note: this will access the predict api path created in app.py
+API_URL = "http://localhost:8501/predict"
+
+st.title("Insurance Premium Category Predictor")
+st.markdown("Enter your details below:")
+
+#Input fields
+age = st.number_input("Age", min_value=1, max_value=119, value=30)
+weight = st.number_input("Weight(kg)", min_value = 1.0, value = 60.0)
+height = st.number_input("Height(m)", min_value= 1.0,max_value=2.7, value=1.87)
+income_lpa = st.number_input("Income (LPA)", min_value = 0.1, value = 10.0)
+smoker = st.selectbox("Do you smoke?",options=[True,False])
+city = st.text_input("City", value = "Mumbai")
+occupation = st.selectbox("Occupation", options=['retired', 'freelancer', 'student', 'government_job', 'business_owner', 'unemployed', 'private_job'])
+
+if st.button("Predict Premium Category"):
+    #store input data in a json
+    input_data = {
+        "age":age,
+        "weight":weight,
+        "height":height,
+        "income_lpa":income_lpa,
+        "smoker":smoker,
+        "city":city,
+        "occupation":occupation
+    }
+
+    try:
+        #send post request to /predict API using input data as json
+        response = requests.post(API_URL, json=input_data)
+        #store the result response
+        result = response.json()
+        print(result)
+
+        #if we received correct response, display the same
+        if response.status_code == 200 and "response" in result:
+            prediction = result['response']
+            st.success(f"Predicted Insurance premium category: **{prediction}")
+        
+        else:
+            st.error(f"API Error: {response.status_code}")
+            st.write(result)
+
+    
+    except requests.exceptions.ConnectionError:
+        st.error("Could not connect to FastAPI server, make sure its running correctly")
